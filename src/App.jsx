0ac049f84d07b24679906cb77f73fd8a3f8c5ef7
AvanishVadke <avanishvadke001@gmail.com>
2024-08-29 14:43:47 +0530

    Routing of pages done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import AppLayout from './layouts/app-layout'
+import LandingPage from './pages/landing'
+import OnBoarding from './pages/onboarding';
+import JobListing from './pages/job-listing';
+import JobPage from './pages/job';
+import PostJob from './pages/post-job';
+import SavedJobs from './pages/save-job';
+import MyJobs from './pages/my-jobs';
 
 function App() {
 
-  return (
-    <div>
-      <button>Login</button>
-    </div>
-  )
+  const router = createBrowserRouter([
+      {
+        element:<AppLayout />,
+        children:[
+          {
+            path: '/',
+            element:<LandingPage />
+          },
+          {
+            path: '/Onboarding',
+            element:<OnBoarding />
+          },
+          {
+            path: '/jobs',
+            element:<JobListing />
+          },
+          {
+            path: '/job/:id',
+            element:<JobPage />
+          },
+          {
+            path: '/post-job',
+            element:<PostJob />
+          },
+          {
+            path: '/saved-job',
+            element:<SavedJobs />
+          },
+          {
+            path: '/my-jobs',
+            element:<MyJobs />
+          },
+
+        ],
+      },
+    ]);
+    
+    return <RouterProvider router={router}/>;
 }
 
 export default App
---
+++ src/pages/job-listing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobListing = () => {
+  return (
+    <div>JobListing</div>
+  )
+}
+
+export default JobListing
